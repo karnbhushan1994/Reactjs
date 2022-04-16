fbc78bf58723c800827a345273978657c31c656a
karnbhushan1994 <karn.becs.13@gmail.com>
2022-04-16 22:30:04 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,70 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import {Link,BrowserRouter as Router,Routes,Route , Navigate,Outlet,useParams} from 'react-router-dom'
-
-ReactDOM.render(
-  <Router>
-     <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/myapps' element={<Navigate to="/learn " />} />
-         <Route path='/learn' element={<Learn />}>
-            <Route path='course' element={<Courses />} >
-               <Route path=':courseid' element={<CourseId />}/>
-            </Route>
-            <Route path='bundle' element={<Bundles />} />
-          </Route>
-     </Routes>
-  </Router>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
-function Home(){
-  return(
-    <div>
-      <h1>Home route</h1>
-    </div>
-  )
-}
-
-function Learn(){
-  return (
-    <div>
-       <h1>course</h1>
-       <h4>All course listed here</h4>
-       <Link className='btn btn-success' to="/learn/course">Course</Link>
-        {" "}
-       <Link className='btn btn-primary' to="/learn/bundle">Bundle</Link>
-    </div>
-  );
-}
-
-function Courses(){
-  return(
-    <div>
-      <h1>Courses</h1>
-      <Outlet/>
-    </div>
-  )
-}
-
-function Bundles(){
-  return(
-    <div>
-      <h1>Bundle</h1>
-    </div>
-  )
-}
-
-function CourseId(){
-   const {courseid} = useParams()
-  return(
-    <div>
-      <h1>Url  parameter id:}{courseid}</h1>
-    </div>
-  )
-}
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
